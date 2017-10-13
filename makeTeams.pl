45720aba69382435dc5e884d9b7c8899c4412d8e
Andrew Williams <stuffmanx@gmail.com>
2017-10-12 19:45:46 -0700

    Comments and cleanup

--- makeTeams.pl
+++ makeTeams.pl
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % CS 352 - Prolog Project
 %
-% Name: PUT YOUR NAME HERE
+% Name: Andrew Williams
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -16,31 +16,53 @@ divideIntoFours([],[]).
 divideIntoFours(INPUT, [PICKED|REST2]) :-
     % teamCompat([A,B,C,D]),
     pickN(4, INPUT, PICKED, UNPICKED),
-    % teamCompat(PICKlED),
+    % teamCompat(PICKED),
     divideIntoFours(UNPICKED, REST2).
-% divideIntoFours().
-%
-%compatible teams
-teamCompat(_, []).
-teamCompat(MEM1, [MEM2|TEAM]) :-
-    compatWith(MEM1, MEM2),
-    compatWith(MEM2, MEM1),
-    teamCompat(MEM1, TEAM).
+
+
 
 /******
- * basic choosing: chooses four compatible members from the given list
+ * test if team is compatible:
+ *
+ * Parameters:
+ *      [A,B,C,D]: List of four team members
  *
  ******/
-%pickN(N, List, Picked, Unpicked)
+teamCompat([A,B,C,D]) :-
+    compatWith(A, B),
+    compatWith(A, C),
+    compatWith(A, D),
+    compatWith(B, C),
+    compatWith(B, D),
+    compatWith(C, D).
+% teamCompat(_, []).
+% teamCompat(MEM1, [MEM2|TEAM]) :-
+%     compatWith(MEM1, MEM2),
+%     compatWith(MEM2, MEM1),
+%     teamCompat(MEM1, TEAM).
 
+
+
+/******
+ * basic picker: chooses N members from the given list
+ *              and returns the rest in their own list.
+ *
+ *  Parameters:
+ *      1: Number to pick from given list
+ *      2: Given List
+ *      3: Picked list
+ *      4: Unpicked list
+ *
+ ******/
 pickN(0, [], [], []).
 pickN(N, [VAL|REST1], [VAL|REST2], REST3) :-
     M is N-1,
-    teamCompat(VAL, REST2), %TODO: Fix this line
+    % teamCompat(VAL, REST2), %TODO: Fix this line
     pickN(M, REST1, REST2, REST3).
 pickN(N, [VAL|REST1], REST2, [VAL|REST3]) :-
     pickN(N, REST1, REST2, REST3).
-% pickN(N, [_|REST1], REST2, REST3) :- pickN(N, REST1, REST2, REST3).
+
+
 
 
 /******
@@ -85,8 +107,8 @@ test4(RESULT) :-
 
 % Test the pickN function
 test5(RESULT, UNPICKED) :-
-    groupOfSize(2, LIST), pickN(1, LIST, RESULT, UNPICKED).
+    groupOfSize(20, LIST), pickN(4, LIST, RESULT, UNPICKED).
 
-% Test teamCompat function
-test6() :-
-    teamCompat(aaron, []).
+% % Test teamCompat function
+% test6() :-
+%     teamCompat(aaron, []).
